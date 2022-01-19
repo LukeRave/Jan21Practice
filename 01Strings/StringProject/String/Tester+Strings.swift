a26744cfe6afda3cac44225d6cb66da79e97782e
Isaac Farr <isaacfarr@Isaacs-MacBook-Air.local>
2022-01-19 11:59:01 -0500

    string tests passing

--- 01Strings/StringProject/String/Tester+Strings.swift
+++ 01Strings/StringProject/String/Tester+Strings.swift
@@ -13,23 +13,35 @@ extension Tester {
     ///
     /// Using print command
     func printWord(word: String) {
+        print(word);
     }
     
     /// input "hello" should print "HELLO"
     func printUpperCase(word: String) {
+        print(word.uppercased());
     }
     
     /// Using for loop, print each char in word
     func printLetters(word: String) {
-
+        for letter in word {
+            print(letter)
+        }
     }
     
     /// Using for-in-enumerated(), print letters at every even-index
     func printEvenIndexEnumerated(word: String) {
+        for (i, letter) in word.enumerated(){
+            if i % 2 == 0 {
+                print(letter)
+            }
+        }
     }
     
     /// Using for-in-stride and string.index, print letters at every even-index
     func printEvenIndexStride(word: String) {
+        for letterindex in stride(from:0, to:word.count, by:2) {
+            print(word[word.index(word.startIndex, offsetBy: letterindex)])
+        }
     }
     
     /**
@@ -41,11 +53,26 @@ extension Tester {
      one -> oneyay
      */
     func printPigLatin(word: String) {
+        let vowels = "aeiou";
+        var result = word;
+        
+        if vowels.contains(result[result.startIndex]){
+            print(result + "yay")
+        }
+        else {
+            let firstLetter = result.removeFirst()
+            print(result + String(firstLetter) + "ay")
+        }
     }
     
     /// Using loop and string.index, returns true if input is a palindrome
     /// Do not use .reversed()
     func isPalindrome(word: String) -> Bool {
-        return false
+        for (loopindex, letter) in word.enumerated() {
+            if letter != word[word.index(word.index(before: word.endIndex), offsetBy: loopindex * -1)] {
+                return false
+            }
+        }
+        return true
     }
 }
