6c248155ca36e698ef9a491e3763206bcb48e137
ravebizz <ravebizz@ravebizzs-MacBook-Air-2.local>
2022-01-19 15:41:55 -0500

    Finished assignment 3

--- 03SetDictionaryOptionals/SetDictionaryProject/SetDictionaryProject/Tester+Optionals.swift
+++ 03SetDictionaryOptionals/SetDictionaryProject/SetDictionaryProject/Tester+Optionals.swift
@@ -12,27 +12,38 @@ extension Tester {
     /// Unwrap using optional binding: if let
     /// else return 0
     func unwrap(optional: Int?) -> Int {
-        return 0
+        if let opt = optional {
+            return opt
+        } else {
+            return 0
+        }
     }
     
     /// Unwrap using optional binding: guard let
     /// else return ""
     func unwrap(optional: String?) -> String {
-        return ""
+        guard let opt = optional else {
+            return ""
+        }
+        return opt
     }
     
     /// Unwrap using nil coalescing
     /// takes optional array, return first value?
     func unwrap(optional: [Int]?) -> Int {
-        
-        return 0
+        let optInt = optional?.first
+        return (optInt ?? 0)
     }
     
     /// Create a url from a string address, avoid force unwrapping
     /// else return the main bundle url
     func createURL(from address: String?) -> URL {
-        
-        
+        if let add = address {
+            let myURL = URL(string: add)
+            if let url = myURL {
+                return url
+            }
+        }
         return Bundle.main.bundleURL
     }
     
@@ -40,41 +51,56 @@ extension Tester {
     /// Input array of [Int?], unwrap each Int? and return the an array of Int
     /// Input [Int?] = [1, nil, nil, 2] -> [1, 2]
     func unwrapElementsInArray(array: [Int?]) -> [Int] {
-        
-        
-        return []
+        var outputArray: [Int] = []
+        for val in array {
+            if let v = val {
+                outputArray.append(v)
+            }
+        }
+        return outputArray
     }
     
     /// Unwrap using nil coalescing
     /// takes optional dictionary, return value at key?
     func unwrap(optional: [Int: Int]?, key: Int?) -> Int {
-        
-        return 0
+        return (optional?[key ?? 0] ?? 0)
     }
     
     /// Unwrap using optional chaining and optional binding
     /// takes nested optional array, return first value?
     func unwrap(optional: [[Int]?]?) -> Int {
-        
-        return 0
+        if let f = optional?[0]?.first {
+            return f
+        } else {
+            return 0
+        }
     }
     
     /// Unwrap using implicitly unwrap
     func implicitUnwrap(optional: Int?) -> Int {
-        
-        return 0
+        return optional!
     }
     
     /// Cast string into non-optional Int
     func castStringIntoInt(string: String) -> Int {
-        
-        return 0
+        if let myInt = Int(string) {
+            return myInt
+        }
+        return Int.min
     }
     
     /// Cast an array of string into an array of Int, remove any nil values
     /// ["1", "5", "apple", "2"] --> [1, 5, 2]
     func castArray(from strArr: [String]) -> [Int] {
-    
-        return []
+        var outputArray: [Int] = []
+        for str in strArr {
+            let temp = castStringIntoInt(string: str)
+            if temp == Int.min {
+                continue
+            }
+            outputArray.append(temp)
+        }
+        
+        return outputArray
     }
 }
--- 03SetDictionaryOptionals/SetDictionaryProject/SetDictionaryProject/Tester+SetDict.swift
+++ 03SetDictionaryOptionals/SetDictionaryProject/SetDictionaryProject/Tester+SetDict.swift
@@ -10,30 +10,74 @@ import Foundation
 struct Tester {
     /// Remove an array with the duplicated values removed
     func removeDups(nums: [Int]) -> [Int] {
-        
-        return []
+        var uniqueNums: Set<Int> = []
+        for num in nums {
+            uniqueNums.insert(num)
+        }
+        var outputArray: [Int] = []
+        for num in uniqueNums {
+            outputArray.append(num)
+        }
+        return outputArray
     }
     
     /// Find the nums that exist in both arrays
     func findCommonNums(from firstNums: [Int], and secondNums: [Int]) -> [Int] {
-        
-        return []
+        var outputArray: [Int] = []
+        for num in firstNums {
+            for num2 in secondNums {
+                if (num == num2) {
+                    outputArray.append(num)
+                }
+            }
+        }
+        return outputArray
     }
     
     /// first: "hello", second: "bye" -> ["e"]
     func findMatchingLetters(firstWord: String, secondWord: String) -> [Character] {
-        return []
+        var outputArray: [Character] = []
+        for char in firstWord {
+            for char2 in secondWord {
+                if (char == char2) {
+                    outputArray.append(char)
+                }
+            }
+        }
+        return outputArray
     }
     
     
     /// Create a dictionary of the counts of the letters, ignoring spaces
     func createCounter(string: String) -> [Character: Int] {
-        return [:]
+        var outputDict: [Character:Int] = [:]
+        for char in string {
+            if (char == " ") {
+                continue
+            }
+            if let kvpair = outputDict[char] {
+                outputDict.updateValue(kvpair+1, forKey: char)
+            } else {
+                outputDict[char] = 1
+            }
+        }
+        return outputDict
     }
     
     /// Find most frequent letter in string: "hello there" -> "e"
     func mostFrequentLetter(string: String) -> Character {
-        return "."
+        var counter = createCounter(string: string)
+        var maxCount = 0
+        var maxKey: Character = "a"
+        for key in counter.keys {
+            if let count = counter[key] {
+                if count > maxCount {
+                    maxCount = count
+                    maxKey = key
+                }
+            }
+        }
+        return maxKey
     }
     
     /// Given a string of numbers: "121" and a sentence "fire water fire"
@@ -42,15 +86,55 @@ struct Tester {
     /// "1122" and a sentence "fire water fire water" (returns false)
     /// "11221" and a sentence "fire fire water water fire" (returns true)
     func translateNums(sequence: String, sentence: String) -> Bool {
-
-        return false
+        var thisWord = ""
+        var words: [String] = []
+        for char in sentence {
+            if (char == " ") {
+                words.append(thisWord)
+                thisWord = ""
+            } else {
+                thisWord.append(char)
+            }
+        }
+        words.append(thisWord)
+        var mapping: [Character:String] = [:]
+        for (ind, char) in sequence.enumerated() {
+            
+            if let value = mapping[char] {
+                if words[ind] != value {
+                    return false
+                }
+            } else {
+                mapping[char] = words[ind]
+            }
+        }
+        return true
     }
     
     /// Find number of pairs that SUMS up to 0
     /// O(n) time: don't use nested for loops
     func findPairsOfOpposites(nums: [Int]) -> Int {
-        
-        return 0
+        var opposites: [Int:Int] = [:]
+        var count = 0
+        for num in nums {
+            if let value = opposites[-num] {
+                if value > 0 {
+                    count += 1
+                    opposites.updateValue(value - 1, forKey: -num)
+                } else {
+                    if let value2 = opposites[num] {                            opposites.updateValue(value2 + 1, forKey: num)
+                    } else {
+                        opposites[num] = 1
+                    }
+                }
+            } else {
+                if let value2 = opposites[num] {                            opposites.updateValue(value2 + 1, forKey: num)
+                } else {
+                    opposites[num] = 1
+                }
+            }
+        }
+        return count
     }
     
     /**
@@ -60,8 +144,26 @@ struct Tester {
      [1, 4, 8, 1] -> [1, 2, 3, 1]
      */
     func reduceDistanceKeepPriority(array: [Int]) -> [Int] {
-        
-        return []
+        var numsSet: Set<Int> = []
+        for num in array {
+            numsSet.insert(num)
+        }
+        var numsSetSorted: [Int] = []
+        for num in numsSet {
+            numsSetSorted.append(num)
+        }
+        numsSetSorted = numsSetSorted.sorted()
+        var reductionMapping: [Int:Int] = [:]
+        for (ind,num) in numsSetSorted.enumerated() {
+            reductionMapping[num] = ind + 1
+        }
+        var outputArray: [Int] = []
+        for num in array {
+            if let red = reductionMapping[num] {
+                outputArray.append(red)
+            }
+        }
+        return outputArray
     }
     
 }
