da4ac7f4895b3601224f4b4b50be7aa6269bc219
ravebizz <ravebizz@ravebizzs-MacBook-Air.local>
2022-01-19 12:02:30 -0500

    First commit

--- 01Strings/StringProject/String/Tester+Strings.swift
+++ 01Strings/StringProject/String/Tester+Strings.swift
@@ -13,23 +13,35 @@ extension Tester {
     ///
     /// Using print command
     func printWord(word: String) {
+        print(word)
     }
     
     /// input "hello" should print "HELLO"
     func printUpperCase(word: String) {
+        print(word.uppercased())
     }
     
     /// Using for loop, print each char in word
     func printLetters(word: String) {
-
+        for char in word {
+            print(char)
+        }
     }
     
     /// Using for-in-enumerated(), print letters at every even-index
     func printEvenIndexEnumerated(word: String) {
+        for (index, char) in word.enumerated() {
+            if (index % 2 == 0) {
+                print(char)
+            }
+        }
     }
     
     /// Using for-in-stride and string.index, print letters at every even-index
     func printEvenIndexStride(word: String) {
+        for index in stride(from: 0, to: word.count, by: 2) {
+            print(word[word.index(word.startIndex, offsetBy: index)])
+        }
     }
     
     /**
@@ -41,11 +53,51 @@ extension Tester {
      one -> oneyay
      */
     func printPigLatin(word: String) {
+        let vowels: [Character] = ["a","e","i","o","u"]
+        let firstLetter = word.first
+        guard let fl = firstLetter else {
+            print("first letter is nil")
+            fatalError()
+        }
+        var outputString = ""
+        if vowels.contains(fl) {
+            outputString.append(fl)
+        }
+        for ind in 1..<word.count {
+            let char = word[word.index(word.startIndex, offsetBy: ind)]
+            outputString.append(char)
+        }
+        if vowels.contains(fl) {
+            outputString.append("yay")
+        } else {
+            outputString.append(fl)
+            outputString.append("ay")
+        }
+        print(outputString)
     }
     
     /// Using loop and string.index, returns true if input is a palindrome
     /// Do not use .reversed()
     func isPalindrome(word: String) -> Bool {
-        return false
+        var firstHalf = ""
+        for (ind, char) in word.enumerated() {
+            if ind == word.count/2 {
+                break
+            }
+            firstHalf.append(char)
+        }
+        var secondHalf = ""
+        if (word.count % 2 == 0) {
+            let hIndex = word.startIndex
+            for ind in stride(from: word.count-1, through: word.count/2, by: -1) {
+                secondHalf.append(word[word.index(hIndex, offsetBy: ind)])
+            }
+        } else {
+            let hIndex = word.startIndex
+            for ind in stride(from: word.count-1, through: word.count/2+1, by: -1) {
+                secondHalf.append(word[word.index(hIndex, offsetBy: ind)])
+            }
+        }
+        return firstHalf == secondHalf
     }
 }
