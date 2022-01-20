87f5db489aa09b579db019a349ea746dba5f67cf
Matthew Flores <ravebizz@Matthews-MacBook-Air.local>
2022-01-19 23:17:39 -0500

    Completed String Tests

--- 01Strings/StringProject/String/Tester+Strings.swift
+++ 01Strings/StringProject/String/Tester+Strings.swift
@@ -13,25 +13,37 @@ extension Tester {
     ///
     /// Using print command
     func printWord(word: String) {
+        print(word)
     }
     
     /// input "hello" should print "HELLO"
     func printUpperCase(word: String) {
+        print(word.uppercased())
     }
     
     /// Using for loop, print each char in word
     func printLetters(word: String) {
-
+        for char in word {
+            print(char)
+        }
     }
     
     /// Using for-in-enumerated(), print letters at every even-index
     func printEvenIndexEnumerated(word: String) {
+        for (index, char) in word.enumerated() {
+            if index % 2 == 0 {
+                print(char)
+            }
+        }
     }
     
     /// Using for-in-stride and string.index, print letters at every even-index
     func printEvenIndexStride(word: String) {
+        for num in stride(from: 0, to: word.count, by: 2){
+            let i = word.index(word.startIndex, offsetBy: num)
+            print(word[i])
+        }
     }
-    
     /**
      Pig Latin:
      1) Move the first letter to the end and add "ay"
@@ -40,12 +52,49 @@ extension Tester {
      2) IF word starts with a vowel, add "yay" to the word
      one -> oneyay
      */
-    func printPigLatin(word: String) {
+    func printPigLatin(word: String){
+        let firstLetter = word[word.startIndex]
+        switch firstLetter {
+        case "a":
+            print(word + "yay")
+            return
+        
+        case "e":
+            print(word + "yay")
+            return
+        
+        case "i":
+            print(word + "yay")
+            return
+        
+        case "o":
+            print(word + "yay")
+            return
+        
+        case "u":
+            print(word + "yay")
+            return
+        
+        default:
+            var wordMinusFist = ""
+            for num in 1...word.count-1 {
+                let nextIndex = word[word.index(word.startIndex, offsetBy: num)]
+                wordMinusFist += "\(nextIndex)"
+            }
+            let pigConst = wordMinusFist + "\(firstLetter)ay"
+            print(pigConst)
+        }
     }
     
     /// Using loop and string.index, returns true if input is a palindrome
     /// Do not use .reversed()
     func isPalindrome(word: String) -> Bool {
-        return false
+        var reverseWord: String = ""
+        let beforeLast = word.index(before: word.endIndex)
+        for num in 0..<word.count {
+            let nextIndex = word[word.index(beforeLast, offsetBy: -num)]
+            reverseWord += "\(nextIndex)"
+        }
+        return reverseWord == word
     }
 }
