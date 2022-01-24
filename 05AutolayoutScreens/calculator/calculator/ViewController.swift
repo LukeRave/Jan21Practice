793ee014f518e20b9963a30a4fb9f70c1b966f5f
Rave Bizz <ravebizz@Raves-Air-7.hsd1.pa.comcast.net>
2022-01-23 20:35:22 -0500

    finished all three auto layout redesigns

---
+++ 05AutolayoutScreens/calculator/calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Rave Bizz on 1/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
