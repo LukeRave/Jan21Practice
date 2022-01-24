3f2512ecaffec105e0faba302814bd4ac12fb1df
ravebizz <ravebizz@ravebizzs-MacBook-Air-2.local>
2022-01-24 11:54:29 -0500

    added functionality required for this assignment

--- 07Enum/enums/enums/TemperatureScale.swift
+++ 07Enum/enums/enums/TemperatureScale.swift
@@ -13,7 +13,22 @@ import Foundation
  3) Implement tempInFahren property
  */
 
-enum TemperatureScale {
+enum TemperatureScale: TempConvertable{
+    func getTempInFahrenheit() -> Int {
+        switch self {
+        case .celcius(let cel):
+            return (cel * 9/5) + 32
+        case .kelvin(let kelv):
+            return (kelv - 273) * 9/5 + 32
+        case .fahrenheit(let fah):
+            return fah
+        }
+    }
+    
+    var tempInFahren: Int {
+        return getTempInFahrenheit()
+    }
+    
     case fahrenheit(Int)
     case celcius(Int)
     case kelvin(Int)
