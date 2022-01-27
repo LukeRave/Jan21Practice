c92b14b6c4d8aa7781c33b33dce223d5540ac983
Shobair Abidi <shobair.abidi@gmail.com>
2022-01-27 12:26:02 -0500

    completed api calls 4/4 passed tests

--- 12NetworkingCall/apiCalls/apiCalls/ViewController.swift
+++ 12NetworkingCall/apiCalls/apiCalls/ViewController.swift
@@ -17,74 +17,93 @@ class ViewController: UIViewController {
     let tvShowsStr = "https://api.tvmaze.com/shows/431/episodes"
     let musicStr = "https://rss.applemarketingtools.com/api/v2/us/music/most-played/10/albums.json"
     
-    /// Display the joke in firstLabel.text
-    /// 1. Make API call to jokes url with URLSession
-    /// 2. Parse the JSON data using Codable protocol
-    /// 3. Update firstLabel in the main thread with text from response
     @IBAction func jokesPressed(_ sender: UIButton) {
-        firstLabel.text = "Testing Joke button"
+        guard let url = URL(string: jokesStr) else {return}
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else {print(error ?? "No Data");return}
+            do {
+                let model = try JSONDecoder().decode(jokeModel.self, from: data)
+                DispatchQueue.main.async {
+                    self.firstLabel.text = model.value!
+                }
+            } catch { print(error) }
+        }
+        task.resume()
     }
-    
     /// Display the first drink's name
     @IBAction func drinksPressed(_ sender: UIButton) {
-        firstLabel.text = "Testing Drinks button"
+        guard let url = URL(string: drinksStr) else {return}
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else {print(error ?? "No Data");return}
+            do {
+                let model = try JSONDecoder().decode(drinkModel.self, from: data)
+                DispatchQueue.main.async {
+                    self.firstLabel.text = model.drinks[0]?.strDrink
+                }
+            } catch { print(error) }
+        }
+        task.resume()
     }
     
     /// Show the name of the first episode
     @IBAction func tvShowsPressed(_ sender: UIButton) {
-        firstLabel.text = "Testing TVShows button"
-        decoding()
-
+        guard let url = URL(string: tvShowsStr) else {return}
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else {print(error ?? "No Data");return}
+            do {
+                let model = try JSONDecoder().decode([Show].self, from: data)
+                DispatchQueue.main.async {
+                    self.firstLabel.text = model.first!.name
+                }
+            } catch { print(error) }
+        }
+        task.resume()
     }
+    
     /// Show the artistName from the first result of the response
     @IBAction func musicPressed(_ sender: UIButton) {
-        firstLabel.text = "Testing music button"
+        guard let url = URL(string: musicStr) else {return}
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else {print(error ?? "No Data");return}
+            do {
+                let model = try JSONDecoder().decode(musicFeed.self, from: data)
+                DispatchQueue.main.async {
+                    self.firstLabel.text = model.feed.results[0].artistName
+                }
+            } catch { print(error) }
+        }
+        task.resume()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureLabels()
     }
-
     func configureLabels() {
         firstLabel.isAccessibilityElement = true
         firstLabel.accessibilityIdentifier = "FirstLabel"
         secondLabel.text = ""
     }
-    
-    weak var luke: ViewController?
-    
-    func testing() {
-        let ituneOne = iTune(feed: SomeObject(title: "", results: [SomeOtherObject(artistName: "asdf")]))
-        
-        ituneOne.feed.results.first?.artistName
-    }
-    
-    func decoding() {
-        let data = Data()
-        do {
-            let decodedShowsArray = try JSONDecoder().decode([Show].self, from: data)
-        } catch {
-            print(error)
-        }
-    }
-    
 }
 
+struct jokeModel: Decodable {
+    let value: String?
+}
+struct Drink: Decodable {
+    let strDrink: String?
+}
+struct drinkModel: Decodable {
+    let drinks: [Drink?]
+}
 struct Show: Decodable {
-    let id: Int?
     let name: String?
 }
-
-struct iTune: Encodable & Decodable {
-    let feed: SomeObject
-}
-
-struct SomeObject: Codable {
-    let title: String?
-    let results: [SomeOtherObject]
-}
-
-struct SomeOtherObject: Codable {
+struct musicData: Decodable {
     let artistName: String?
+}
+struct musicFeedData: Decodable {
+    let results: [musicData]
+}
+struct musicFeed: Decodable {
+    let feed: musicFeedData
 }
