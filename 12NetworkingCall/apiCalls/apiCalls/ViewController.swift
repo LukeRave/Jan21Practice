8949db2b6b40f2abeea4d49b0ff166af23216df6
Matthew Flores <mattmflores@outlook.com>
2022-01-30 22:47:51 -0500

    Completed API Tests

--- 12NetworkingCall/apiCalls/apiCalls/ViewController.swift
+++ 12NetworkingCall/apiCalls/apiCalls/ViewController.swift
@@ -23,20 +23,94 @@ class ViewController: UIViewController {
     /// 3. Update firstLabel in the main thread with text from response
     @IBAction func jokesPressed(_ sender: UIButton) {
         firstLabel.text = "Testing Joke button"
+        guard let url = URL(string: jokesStr) else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url, completionHandler: {(data, response, error) in
+            guard let data = data else {
+                return
+            }
+            do {
+                let parsed = try JSONDecoder().decode(JokeModel.self, from: data)
+                DispatchQueue.main.async {
+                    self.firstLabel.text = parsed.value
+                }
+            }  catch {
+                print(error)
+            }
+        })
+        task.resume()
     }
     
     /// Display the first drink's name
     @IBAction func drinksPressed(_ sender: UIButton) {
         firstLabel.text = "Testing Drinks button"
+        guard let url = URL(string: drinksStr) else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url, completionHandler: {(data, response, error) in
+            guard let data = data else {
+                return
+            }
+            do {
+                let parsed = try JSONDecoder().decode(DrinkParent.self, from: data)
+                DispatchQueue.main.async {
+                    self.firstLabel.text = parsed.drinks[0].strDrink
+                }
+            }  catch {
+                print(error)
+            }
+        })
+        task.resume()
     }
     
     /// Show the name of the first episode
     @IBAction func tvShowsPressed(_ sender: UIButton) {
         firstLabel.text = "Testing TVShows button"
+<<<<<<< HEAD
+=======
+        guard let url = URL(string: tvShowsStr) else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url, completionHandler: {(data,response,error) in
+            guard let data = data else {
+                return
+            }
+            do {
+                let parsed = try JSONDecoder().decode([TVModel].self, from: data)
+                DispatchQueue.main.async {
+                    self.firstLabel.text = parsed[0].name
+                }
+            } catch {
+                print(error)
+            }
+        })
+        task.resume()
+
+>>>>>>> f6a7e57 (Completed API Calls)
     }
     /// Show the artistName from the first result of the response
     @IBAction func musicPressed(_ sender: UIButton) {
         firstLabel.text = "Testing music button"
+        guard let url = URL(string: musicStr) else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url, completionHandler: {(data,response,error) in
+            guard let data = data else {
+                return
+            }
+            do {
+                let parsed = try JSONDecoder().decode(MusicParent.self, from: data)
+                DispatchQueue.main.async {
+                    self.firstLabel.text = parsed.feed.results[0].artistName
+                }
+            } catch {
+                print(error)
+            }
+        })
+        task.resume()
     }
     
     override func viewDidLoad() {
@@ -49,5 +123,35 @@ class ViewController: UIViewController {
         firstLabel.accessibilityIdentifier = "FirstLabel"
         secondLabel.text = ""
     }
-    
 }
+
+struct JokeModel: Decodable {
+    let value: String?
+}
+
+struct Drink: Decodable {
+    let strDrink: String?
+}
+
+struct DrinkParent: Decodable {
+    let drinks: Array<Drink>
+}
+
+struct TVModel: Decodable {
+    let name: String?
+}
+
+struct MusicResponse: Decodable {
+    let results: Array<Results>
+}
+
+struct Results: Decodable {
+    let artistName: String?
+}
+
+struct MusicParent: Decodable {
+    let feed: MusicResponse
+}
+
+
+
