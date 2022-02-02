0b4979aa252076e41f08e99ab2179d20d219aa5d
edward <edward.perkins25@gmail.com>
2022-02-02 11:07:12 -0500

    uploaded demo

---
+++ colors MVC/colors MVC/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  colors MVC
+//
+//  Created by Rave Bizz on 2/1/22.
+//
+
+import Foundation
+
+final class Constants {
+    ///add the values of red green and blue at the end of the base url seperated by commas
+    ///to perform a successful url call
+    ///documentation url  ->  https://www.thecolorapi.com/docs
+    ///example url ->  https://www.thecolorapi.com/id?rgb=rgb=0,71,171
+    static let baseUrl = "https://www.thecolorapi.com/id?rgb="
+}
