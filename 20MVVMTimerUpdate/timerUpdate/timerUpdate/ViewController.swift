f49570556f95b32cc207e786afbfdb75b56b6d30
Luat <luat@ravebizz.com>
2022-02-03 10:45:52 -0500

    mvvm practice

---
+++ 20MVVMTimerUpdate/timerUpdate/timerUpdate/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  timerUpdate
+//
+//  Created by Luat on 2/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    private var accessCodeLabel = UILabel()
+    private var viewModel = AccessCodeViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupLabel()
+
+    }
+
+
+    func setupLabel() {
+        view.addSubview(accessCodeLabel)
+        accessCodeLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            accessCodeLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
+            accessCodeLabel.topAnchor.constraint(equalTo: view.centerYAnchor, constant: -15),
+            view.trailingAnchor.constraint(equalTo: accessCodeLabel.trailingAnchor, constant: 20),
+        ])
+        
+        accessCodeLabel.text = "Your access code"
+        accessCodeLabel.textAlignment = .center
+        accessCodeLabel.font = UIFont(name: "HelveticaNeue-Thin", size: 30)
+        accessCodeLabel.backgroundColor = .systemTeal
+    }
+
+}
+
