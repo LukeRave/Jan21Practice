5fd55bb9a47b9be023f3709eeb72e470ae7870d9
Shobair Abidi <shobair.abidi@gmail.com>
2022-02-09 13:16:15 -0500

    forgot to add difficulty estimate [3]

--- makeupFinder copy/makeupFinder/Task X).swift
+++ makeupFinder copy/makeupFinder/Task X).swift
@@ -8,8 +8,5 @@
 import Foundation
 /*
  
- 
- 4. when a user adds a product to favorites from the home and then follows clicks view favorites from the alert modal and the app crashes
- 
  please give a ruff estimate of how hard each task is on a scale of 1-10 then commit ^-^b
  */
