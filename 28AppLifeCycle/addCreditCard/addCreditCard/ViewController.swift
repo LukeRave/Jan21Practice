7ff8d8e7c2e9d1cee9faf223db44ada67e1cdd92
iGFarr <isaacgfarr@gmail.com>
2022-02-10 14:17:05 -0500

    Just saving my layout

--- 28AppLifeCycle/addCreditCard/addCreditCard/ViewController.swift
+++ 28AppLifeCycle/addCreditCard/addCreditCard/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var cards = Array<Int>()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        cards.append(1)
     }
 
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cards.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = String(cards[indexPath.row])
+        return cell;
+    }
+    
+    
+}
